b1795b229a0b22dce7bb555b7fe9f40a4f069c70
nfelipereal <91770423+nfelipereal@users.noreply.github.com>
2024-04-06 23:37:31 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,11 +22,28 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 # Instalar LXDE e XRDP
 sudo apt-get install lxde xrdp -y
 
+# Gerar senha aleatória
+SENHA=$(openssl rand -base64 12)
+
 # Configurar senha do usuário automaticamente
-echo -e "41258\n41258" | sudo passwd ubuntu
+echo -e "$SENHA\n$SENHA" | sudo passwd ubuntu
 
 # Habilitar o serviço XRDP para iniciar com o sistema
 sudo systemctl enable xrdp
 
 # Reiniciar o serviço XRDP para aplicar as configurações
 sudo systemctl restart xrdp
+
+# Mensagem de sucesso e exibição do IP público
+echo -e "\n\nINSTALADO COM SUCESSO\n"
+echo "  _____ __  __  _____  _    _ "
+echo " |_   _|  \/  |/ ____|| |  | |"
+echo "   | | | \  / || (___  | |__| |"
+echo "   | | | |\/| |\___ \ |  __  |"
+echo "  _| |_| |  | |____) || |  | |"
+echo " |_____|_|  |_|_____/ |_|  |_|"
+echo -e "\n\nseu usuario: ubuntu\nsenha: $SENHA\n\nAcesse a conexão remota com o IP público abaixo:\n"
+
+# Exibir o IP público da máquina
+IP_PUBLICO=$(curl -s ifconfig.me)
+echo "IP público da máquina: $IP_PUBLICO"
